d30a5e0ecc764251fb79d97185cf3955ddba8c15
Md Moksedul Momin <63648257+mommy003@users.noreply.github.com>
2023-07-06 10:30:46 +0930

    Add files via upload

--- R/auc_var.r
+++ R/auc_var.r
@@ -24,6 +24,8 @@
   #' dat=dat1 #(this example embedded within the package)
   #' nv=length(dat$V1)
   #' kv=sum(dat$V1)/length(dat$V1)  # pop. prevalence
+  #' #kv is 0.10 for the pre-adjusted phenotype
+  #' #please see dat2 embedded within the package) 
   #' v1=c(1)
   #' output=auc_var(dat,v1,nv,kv)
   #' 
